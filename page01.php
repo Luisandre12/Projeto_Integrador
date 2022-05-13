0b701fb60dfa34d9b9fa30095e8b2bf8f4039c48
Luisandre12 <99758302+Luisandre12@users.noreply.github.com>
2022-05-13 14:22:06 -0300

    site da aula de PHP

---
+++ page01.php
@@ -0,0 +1,27 @@
+<?php
+
+//Criar uma variavél
+$carro = "Fusca";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php echo $carro ?>
+<?php // primeira linha <html> ?>
+<?php //primeira diretiva http <!doctype html ?>
+<?php //comentario em php ?>
+<?php //isso aqui e uma imagem aleatoria ?>
+<img src="https://api.lorem.space/image/movie?w=150&h=220&t=1" > 
+    
+</body>
+</html>
+<?php // notação camelcase = corDoCarro = "azul" ?>
+<?php // notação pascal = cor_do_carro  = "azul"?>
