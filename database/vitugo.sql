8c0818727053daf0fe6395920ba18c0974a5c003
Luisandre12 <99758302+Luisandre12@users.noreply.github.com>
2022-05-18 15:17:07 -0300

    Update vitugo.sql

--- database/vitugo.sql
+++ database/vitugo.sql
@@ -69,16 +69,16 @@ INSERT INTO users (
     user_type
 ) VALUES 
 (
-    'Setembrino Trocatapas',
-    'set@brino.com',
-    '1990-10-14',
-    'https://randomuser.me/api/portraits/lego/6.jpg',
-    'Programador, agricultor, enrolador, fazedor de nada.',
+    'Instituto Ayrton Senna',
+    'institutoayrtonsenna@gmail.com',
+    '1994-11-20',
+    '/img/articles/logo-B.jpg',
+    'Sede social:Pinheiros, São Paulo.',
     SHA1('Qw3rtyui0P'),
     'author'
 ),
 (
-    'Vagalume',
+     'Vagalume',
     'falecom@vagalume.org.br',
     '2002-01-11',
     '/img/articles/art 02.png',
@@ -86,7 +86,7 @@ INSERT INTO users (
     SHA1('Qw3rtyui0P'),
     'author'
 ),
-(
+( 
     'Amigos do Bem',
     'informacoes@amigosdobem.org',
     '1993-05-02',
@@ -96,20 +96,29 @@ INSERT INTO users (
     'admin'
 ),
 (
-    'Juanita Malita',
-    'nitamalita@gmail.com',
-    '1991-10-12',
-    'https://randomuser.me/api/portraits/lego/1.jpg',
-    'Motorista, eletricista, ortopedista e horista.',
+    'Instituto 5 Elementos',
+    'comunicacao@5elementos.org.br',
+    '1993-01-01',
+    '/img/articles/aa.png',
+    'Sede social:São Paulo, SP.',
     SHA1('Qw3rtyui0P'),
     'author'
 ),
 (
-    'Avante',
+      'Avante',
     'avante@avante.org.br',
     '1991-11-01',
     '/img/articles/Avanteperfil.png',
     'Rua Almirante Barroso, nº 64, Rio Vermelho, Salvador/ BA, CEP: 41.950-355.',
+    SHA1('Qw3rtyui0P'),
+    'author'
+),
+(
+    'Cidadão Pró-Mundo',
+    'comunicacao@cidadaopromundo.org.',
+    '1997-01-01',
+    '/img/articles/5a5bd129447d7.jpg',
+    'R. Pref. Passos, 95 - Vila Itapura, Campinas - SP.',
     SHA1('Qw3rtyui0P'),
     'author'
 );
@@ -132,26 +141,7 @@ Trabalham com educadores, administradores e pesquisadores para preparar criança
     '1'
 ),
 (
-    'Veja como isso é feito',
-    'Aprenda a fazer da forma certa o que você sempre fez errado.',
-    '/img/articles/2.jpg',
-    '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto fugiat, nesciunt iure porro aliquid id consequuntur nisi placeat assumenda vero magni repellendus possimus corporis sed, quaerat totam? Veniam, pariatur ex?</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore sunt quo nam commodi repudiandae voluptas excepturi est aut iste veniam. Itaque possimus ullam eius quaerat? Quam aliquam eius corporis ut!</p><img src="https://picsum.photos/400/300" alt="Imagem aleátória"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quisquam amet, sunt, magnam debitis aliquam voluptates eius veniam explicabo molestias enim illo quam exercitationem ducimus vel eos eligendi aut libero.</p><ul><li><a href="https://guithub.com/Luferat">GitHub do Fessô</a></li><li><a href="http://catabits.com.br">Blog do Fessô</a></li><li><a href="https://bit.ly/LuferatMeet">Google Meet do Fessô</a></li></ul><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At itaque deserunt perferendis sit voluptatum sunt, minus labore ratione modi nesciunt distinctio temporibus amet omnis sapiente, dicta repudiandae ipsum, eaque deleniti!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas quos quasi quis pariatur iure officia ab, eius beatae, fuga, in dolores neque possimus necessitatibus nostrum nulla expedita tempore harum tenetur?</p>',
-    '2'
-),
-(
-    'Comprando o que não pode ser vendido',
-    'Neste artigo aprenderemos a fazer uma coisa que ninguém faz.',
-    '/img/articles/3.jpg',
-    '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto fugiat, nesciunt iure porro aliquid id consequuntur nisi placeat assumenda vero magni repellendus possimus corporis sed, quaerat totam? Veniam, pariatur ex?</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore sunt quo nam commodi repudiandae voluptas excepturi est aut iste veniam. Itaque possimus ullam eius quaerat? Quam aliquam eius corporis ut!</p><img src="https://picsum.photos/400/300" alt="Imagem aleátória"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quisquam amet, sunt, magnam debitis aliquam voluptates eius veniam explicabo molestias enim illo quam exercitationem ducimus vel eos eligendi aut libero.</p><ul><li><a href="https://guithub.com/Luferat">GitHub do Fessô</a></li><li><a href="http://catabits.com.br">Blog do Fessô</a></li><li><a href="https://bit.ly/LuferatMeet">Google Meet do Fessô</a></li></ul><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At itaque deserunt perferendis sit voluptatum sunt, minus labore ratione modi nesciunt distinctio temporibus amet omnis sapiente, dicta repudiandae ipsum, eaque deleniti!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas quos quasi quis pariatur iure officia ab, eius beatae, fuga, in dolores neque possimus necessitatibus nostrum nulla expedita tempore harum tenetur?</p>',
-    '2'
-    'Primeiro artigo',
-    'Como fazer alguma coisa que você não sabe fazer.',
-    '/img/articles/1.jpg',
-    '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto fugiat, nesciunt iure porro aliquid id consequuntur nisi placeat assumenda vero magni repellendus possimus corporis sed, quaerat totam? Veniam, pariatur ex?</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore sunt quo nam commodi repudiandae voluptas excepturi est aut iste veniam. Itaque possimus ullam eius quaerat? Quam aliquam eius corporis ut!</p><img src="https://picsum.photos/400/300" alt="Imagem aleátória"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quisquam amet, sunt, magnam debitis aliquam voluptates eius veniam explicabo molestias enim illo quam exercitationem ducimus vel eos eligendi aut libero.</p><ul><li><a href="https://guithub.com/Luferat">GitHub do Fessô</a></li><li><a href="http://catabits.com.br">Blog do Fessô</a></li><li><a href="https://bit.ly/LuferatMeet">Google Meet do Fessô</a></li></ul><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At itaque deserunt perferendis sit voluptatum sunt, minus labore ratione modi nesciunt distinctio temporibus amet omnis sapiente, dicta repudiandae ipsum, eaque deleniti!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas quos quasi quis pariatur iure officia ab, eius beatae, fuga, in dolores neque possimus necessitatibus nostrum nulla expedita tempore harum tenetur?</p>',
-    '1'
-),
-(
-    'Vagalume',
+     'Vagalume',
     'Torne-se um vaga-lume, contribua mensalmente com a nossa organização.',
     '/img/articles/Vagalume.png',
     '<p>Sylvia, Laís e Fofa queriam conhecer mais sobre o país em que viviam e, por isso, escolheram ir para a maior região do Brasil: a Amazônia Legal brasileira, que ocupa 60% do nosso território. Mas elas não queriam ir, aprender, conhecer e não deixar algo para quem elas conheceriam no caminho. Queriam trocar, aprender e ensinar, e foi aí que tiveram a ideia de levar livros e formar mediadores de leitura pelos lugares onde passassem.</p>
@@ -161,7 +151,7 @@ Trabalham com educadores, administradores e pesquisadores para preparar criança
     '2'
 ),
 (
-    'AMIGOS DO BEM',
+  'AMIGOS DO BEM',
     'Se não posso fazer tudo o que devo, devo, ao menos, fazer tudo o que posso!.',
     '/img/articles/Amigosdobem.png',
     '<p>O nosso trabalho começou em 1993, com um pequeno grupo de amigos, liderado por Alcione Albanesi. Hoje, somos um dos maiores projetos sociais do país e atendemos, regularmente, a mais de 150 mil pessoas no sertão de Alagoas, de Pernambuco e do Ceará.</p>
@@ -187,26 +177,7 @@ Trabalhamos com e para crianças, jovens, grupos de mulheres, professores, educa
 '4'
 ),
 (
-    'Projeto Uerê',
-    'NÓS ACREDITAMOS QUE TODOS SÃO CAPAZES DE APRENDER.',
-    '/img/articles/5.jpg',
-    '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto fugiat, nesciunt iure porro aliquid id consequuntur nisi placeat assumenda vero magni repellendus possimus corporis sed, quaerat totam? Veniam, pariatur ex?</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore sunt quo nam commodi repudiandae voluptas excepturi est aut iste veniam. Itaque possimus ullam eius quaerat? Quam aliquam eius corporis ut!</p><img src="https://picsum.photos/400/300" alt="Imagem aleátória"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quisquam amet, sunt, magnam debitis aliquam voluptates eius veniam explicabo molestias enim illo quam exercitationem ducimus vel eos eligendi aut libero.</p><ul><li><a href="https://guithub.com/Luferat">GitHub do Fessô</a></li><li><a href="http://catabits.com.br">Blog do Fessô</a></li><li><a href="https://bit.ly/LuferatMeet">Google Meet do Fessô</a></li></ul><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At itaque deserunt perferendis sit voluptatum sunt, minus labore ratione modi nesciunt distinctio temporibus amet omnis sapiente, dicta repudiandae ipsum, eaque deleniti!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas quos quasi quis pariatur iure officia ab, eius beatae, fuga, in dolores neque possimus necessitatibus nostrum nulla expedita tempore harum tenetur?</p>',
-    '2'
-),
-(
-    'Cidadão Pró-Mundo',
-    'Ensinar inglês e transformar a sociedade.',
-    '/img/articles/5a5bd129447d7.jpg',
-    '<p>O Cidadão Pró-Mundo tem como objetivo proporcionar oportunidades educacionais para jovens e adultos em comunidades carentes, e sua missão é promover oportunidades de inclusão e inclusão na sociedade brasileira por meio do ensino de inglês.</p><p>A Cidadão Pró-Mundo foi reconhecida como uma das 100 melhores ONGs do Brasil por sua estrutura administrativa, operacional e de governança altamente organizada e o mais moderno modelo corporativo, selecionada como a melhor ONG educacional do Brasil 2020, sendo considerada a maior e mais bem estruturada ONG de ensino de inglês que atua no Brasil.</p><img src="/img/articles/foto_TC.PNG-500x300.png" alt="Imagem aleátória"><p>Após 25 anos de atuação e contando com uma metodologia inovadora de voluntariado e ensino, atualmente a CPM conta com cerca de 1.400 voluntários, entre professores e equipe administrativa, além de uma equipe contratada dedicada às áreas estratégicas da ONG (Comunicação, Financeiro,  Operações e Pedagógico), todos mobilizados para atender a seus mais de 2.500 beneficiários: alunos que assistem ao curso de inglês em uma de nossas 12 Unidades de Ensino.</p>',
-    '6'
-    'É assim que se faz isso',
-    'Aprenda como fazer para apreder o que fazer, sabendo o que fazer.',
-    '/img/articles/4.jpg',
-    '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto fugiat, nesciunt iure porro aliquid id consequuntur nisi placeat assumenda vero magni repellendus possimus corporis sed, quaerat totam? Veniam, pariatur ex?</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore sunt quo nam commodi repudiandae voluptas excepturi est aut iste veniam. Itaque possimus ullam eius quaerat? Quam aliquam eius corporis ut!</p><img src="https://picsum.photos/400/300" alt="Imagem aleátória"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quisquam amet, sunt, magnam debitis aliquam voluptates eius veniam explicabo molestias enim illo quam exercitationem ducimus vel eos eligendi aut libero.</p><ul><li><a href="https://guithub.com/Luferat">GitHub do Fessô</a></li><li><a href="http://catabits.com.br">Blog do Fessô</a></li><li><a href="https://bit.ly/LuferatMeet">Google Meet do Fessô</a></li></ul><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At itaque deserunt perferendis sit voluptatum sunt, minus labore ratione modi nesciunt distinctio temporibus amet omnis sapiente, dicta repudiandae ipsum, eaque deleniti!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas quos quasi quis pariatur iure officia ab, eius beatae, fuga, in dolores neque possimus necessitatibus nostrum nulla expedita tempore harum tenetur?</p>',
-    '4'
-),
-(
-    'Avante',
+  'Avante',
     'O Olhar do Adolescente focalizando a escola.',
     '/img/articles/Avante.png',
     '<p>A instituição nasceu do sonho de um grupo de profissionais liderados por Ana Luiza Buratto e Maria Thereza Marcílio, de contribuir para a construção de uma sociedade mais justa e solidária, calcada no diálogo, na diversidade e na valorização do coletivo. Por meio da consolidação e conquista de parcerias, vem realizando ações de reconhecimento e valorização da escola pública, fortalecimento à empreendimentos solidários, articulação e formação de conselheiros de direito e escuta e participação política de crianças.</p>
@@ -216,9 +187,9 @@ Trabalhamos com e para crianças, jovens, grupos de mulheres, professores, educa
     '5'
 ),
 (
-    'Cortes cortados de cantos arredondados',
-    'Neste artigo aprenderemos a fazer cantos de cortes cortados.',
-    '/img/articles/6.jpg',
-    '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto fugiat, nesciunt iure porro aliquid id consequuntur nisi placeat assumenda vero magni repellendus possimus corporis sed, quaerat totam? Veniam, pariatur ex?</p><p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore sunt quo nam commodi repudiandae voluptas excepturi est aut iste veniam. Itaque possimus ullam eius quaerat? Quam aliquam eius corporis ut!</p><img src="https://picsum.photos/400/300" alt="Imagem aleátória"><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quisquam amet, sunt, magnam debitis aliquam voluptates eius veniam explicabo molestias enim illo quam exercitationem ducimus vel eos eligendi aut libero.</p><ul><li><a href="https://guithub.com/Luferat">GitHub do Fessô</a></li><li><a href="http://catabits.com.br">Blog do Fessô</a></li><li><a href="https://bit.ly/LuferatMeet">Google Meet do Fessô</a></li></ul><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At itaque deserunt perferendis sit voluptatum sunt, minus labore ratione modi nesciunt distinctio temporibus amet omnis sapiente, dicta repudiandae ipsum, eaque deleniti!</p><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas quos quasi quis pariatur iure officia ab, eius beatae, fuga, in dolores neque possimus necessitatibus nostrum nulla expedita tempore harum tenetur?</p>',
-    '4'
+    'Cidadão Pró-Mundo',
+    'Ensinar inglês e transformar a sociedade.',
+    '/img/articles/5a5bd129447d7.jpg',
+    '<p>O Cidadão Pró-Mundo tem como objetivo proporcionar oportunidades educacionais para jovens e adultos em comunidades carentes, e sua missão é promover oportunidades de inclusão e inclusão na sociedade brasileira por meio do ensino de inglês.</p><p>A Cidadão Pró-Mundo foi reconhecida como uma das 100 melhores ONGs do Brasil por sua estrutura administrativa, operacional e de governança altamente organizada e o mais moderno modelo corporativo, selecionada como a melhor ONG educacional do Brasil 2020, sendo considerada a maior e mais bem estruturada ONG de ensino de inglês que atua no Brasil.</p><img src="/img/articles/foto_TC.PNG-500x300.png" alt="Imagem aleátória"><p>Após 25 anos de atuação e contando com uma metodologia inovadora de voluntariado e ensino, atualmente a CPM conta com cerca de 1.400 voluntários, entre professores e equipe administrativa, além de uma equipe contratada dedicada às áreas estratégicas da ONG (Comunicação, Financeiro,  Operações e Pedagógico), todos mobilizados para atender a seus mais de 2.500 beneficiários: alunos que assistem ao curso de inglês em uma de nossas 12 Unidades de Ensino.</p>',
+    '6'
 );
